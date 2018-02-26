7f07b5bb733454f7bfe7f2d1d7a262a7cf7f4a08
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2018-02-26 01:05:14 -0700

    commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ export default class Header extends Component{
             <nav className='header'>
                 <Link to='/' style={{ textDecoration: 'none', color: 'white' }}><div className='homelink'>Home</div></Link>
                 <Link to='/createaplaylist' style={{ textDecoration: 'none', color: 'white' }}><div>Create-A-Playlist</div></Link>
-                <Link to='/' style={{ textDecoration: 'none', color: 'white' }}><h1>JD</h1></Link>
+                <h1 className="projectsTitle">Projects</h1>
                 <Link to='/tybl' style={{ textDecoration: 'none', color: 'white' }}><div>Tybl</div></Link>
                 <Link to='/groupproject' style={{ textDecoration: 'none', color: 'white' }}><div>Group Project</div></Link>
             </nav>
