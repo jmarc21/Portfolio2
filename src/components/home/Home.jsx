a86c70f3b2842848f31584ba06160a9e0646343c
Justin DeMarco <justindemarco21@gmail.com>
2018-03-17 13:31:15 -0600

    fixes to the home style

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -169,13 +169,6 @@ export default class Home extends Component {
         smoothScroll.scrollTo(id)
     }
     render() {
-        window.onscroll = function () {
-            if (document.body.scrollTop > 725 || document.documentElement.scrollTop > 725) {
-                this.setState({
-                    skillsAnimaton: true,
-                })
-            }
-        }.bind(this)
         return (
             <div style={{ backgroundColor: "black" }}>
                 {this.state.animateLoad ?
@@ -209,7 +202,6 @@ export default class Home extends Component {
                                 <br />
                                 <h1 className="skillstitle" >About Me</h1>
                                 <div className="divider"></div>
-                                {/* <img src={mepic} alt="" className="aboutmepic" /> */}
                                 <p className="aboutmecontext">
                                     I'm Justin, A Web Developer from Utah. <br />
                                     I pursued Web Development because I have a passion to engineer things, as well as technology and business. <br />
@@ -226,114 +218,107 @@ export default class Home extends Component {
                                 </div>
                             </div>
                             <div className="skillsContainer" id="skills" >
-                                {/* <div className={this.state.skillsAnimaton ? 'skills fadeup' : 'skills'}  > */}
-                                    <h1 className='skillstitle'>Skills</h1>
-                                    <div className="divider"></div>
-                                    <div className='skill'>
-                                        <div className='iconcontainer'>
-                                            <img src="https://wmira.gallerycdn.vsassets.io/extensions/wmira/react-playground-vscode/0.0.11/1494599205180/Microsoft.VisualStudio.Services.Icons.Default" alt="" className="icon" />
-                                            <div className='iconname'>React</div>
-                                        </div>
-                                        <div className='iconcontainer'>
-                                            <img src="https://cdn.worldvectorlogo.com/logos/angular-icon-1.svg" alt="" className="icon" />
-                                            <div className='iconname'>Angular</div>
-                                        </div>
-                                        <div className='iconcontainer'>
-                                            <img src="https://chapmanjeff.github.io/portfolio/img/portfolio/thumbnails/JavaScript-logo.png" alt="" className="icon" />
-                                            <div className='iconname'>JavaSript</div>
-                                        </div>
-                                        <div className='iconcontainer'>
-                                            <img src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png" alt="" className="icon" />
-                                            <div className='iconname'>Node.js</div>
-                                        </div>
-                                        <div className='iconcontainer'>
-                                            <img src="https://tctechcrunch2011.files.wordpress.com/2012/09/html5_badge_512.png" alt="" className="icon" />
-                                            <div className='iconname'>HTML5</div>
-                                        </div>
-                                        <div className="iconcontainer">
-                                            <img src="https://www.shareicon.net/download/2015/09/08/97876_css_512x512.png" alt="" className="icon" />
-                                            <div className='iconname'>CSS3</div>
-                                        </div>
-                                        <div className="iconcontainer">
-                                            <img src="http://jamesdcarlson.com/images/api.svg" alt="" className="icon" />
-                                            <div className='iconname'>RESTful</div>
-                                        </div>
-                                        <div className="iconcontainer">
-                                            <img src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png" alt="" className="icon" />
-                                            <div className='iconname'>Redux</div>
-                                        </div>
-                                        <div className="iconcontainer">
-                                            <img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/landscape/expressjslogo.png" alt="" className="icon" />
-                                            <div className='iconname'>Express</div>
-                                        </div>
-                                        <div className="iconcontainer">
-                                            <img src="http://jamesdcarlson.com/images/git.svg" alt="" className="icon" />
-                                            <div className='iconname'>GitHub</div>
-                                        </div>
-                                        <div className="iconcontainer">
-                                            <img src="http://jamesdcarlson.com/images/postgresql.svg" alt="" className="icon" />
-                                            <div className='iconname'>PostgresQL</div>
-                                        </div>
+                                <h1 className='skillstitle'>Skills</h1>
+                                <div className="divider"></div>
+                                <div className='skill'>
+                                    <div className='iconcontainer'>
+                                        <img src="https://wmira.gallerycdn.vsassets.io/extensions/wmira/react-playground-vscode/0.0.11/1494599205180/Microsoft.VisualStudio.Services.Icons.Default" alt="" className="icon" />
+                                        <div className='iconname'>React</div>
                                     </div>
+                                    <div className='iconcontainer'>
+                                        <img src="https://cdn.worldvectorlogo.com/logos/angular-icon-1.svg" alt="" className="icon" />
+                                        <div className='iconname'>Angular</div>
+                                    </div>
+                                    <div className='iconcontainer'>
+                                        <img src="https://chapmanjeff.github.io/portfolio/img/portfolio/thumbnails/JavaScript-logo.png" alt="" className="icon" />
+                                        <div className='iconname'>JavaSript</div>
+                                    </div>
+                                    <div className='iconcontainer'>
+                                        <img src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png" alt="" className="icon" />
+                                        <div className='iconname'>Node.js</div>
+                                    </div>
+                                    <div className='iconcontainer'>
+                                        <img src="https://tctechcrunch2011.files.wordpress.com/2012/09/html5_badge_512.png" alt="" className="icon" />
+                                        <div className='iconname'>HTML5</div>
+                                    </div>
+                                    <div className="iconcontainer">
+                                        <img src="https://www.shareicon.net/download/2015/09/08/97876_css_512x512.png" alt="" className="icon" />
+                                        <div className='iconname'>CSS3</div>
+                                    </div>
+                                    <div className="iconcontainer">
+                                        <img src="http://jamesdcarlson.com/images/api.svg" alt="" className="icon" />
+                                        <div className='iconname'>RESTful</div>
+                                    </div>
+                                    <div className="iconcontainer">
+                                        <img src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png" alt="" className="icon" />
+                                        <div className='iconname'>Redux</div>
+                                    </div>
+                                    <div className="iconcontainer">
+                                        <img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/landscape/expressjslogo.png" alt="" className="icon" />
+                                        <div className='iconname'>Express</div>
+                                    </div>
+                                    <div className="iconcontainer">
+                                        <img src="http://jamesdcarlson.com/images/git.svg" alt="" className="icon" />
+                                        <div className='iconname'>GitHub</div>
+                                    </div>
+                                    <div className="iconcontainer">
+                                        <img src="http://jamesdcarlson.com/images/postgresql.svg" alt="" className="icon" />
+                                        <div className='iconname'>PostgresQL</div>
+                                    </div>
+                                </div>
                                 {/* </div> */}
                             </div>
-                            <br />
-                            <br />
-                            <div id="projects"></div>
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                            <h1 className='projecttitle'>My Work</h1>
-                            <div className="divider"></div>
-                            <div className="projects">
-                                <div className="project">
-                                    <h1 className='projectName'>Create-A-Playlist</h1>
-                                    <img src={laptop} alt="" className="laptop" />
-                                    <div onMouseOver={() => this.showCAPdetails()} onMouseOut={() => this.showCAPdetailsOut()}>
-                                        {this.state.CAPdetails ?
-                                            <img className="CAPvid" src="https://media.giphy.com/media/X96vlArkPHSbNUewCj/giphy.gif"
-                                                alt="Create-A-Playlist GIF" /> :
-                                            <Link to="/createaplaylist"><div>
+                            <div className='aboutmecontainer' id="projects">
+                                <h1 className='projecttitle'>My Work</h1>
+                                <div className="divider"></div>
+                                <div className="projects">
+                                    <div className="project">
+                                        <h1 className='projectName'>Create-A-Playlist</h1>
+                                        <img src={laptop} alt="" className="laptop" />
+                                        <div onMouseOver={() => this.showCAPdetails()} onMouseOut={() => this.showCAPdetailsOut()}>
+                                            {this.state.CAPdetails ?
                                                 <img className="CAPvid" src="https://media.giphy.com/media/X96vlArkPHSbNUewCj/giphy.gif"
-                                                    alt="Create-A-Playlist GIF" />
-                                                <div className="CAPdetails">Built with <br /> React, Node, <br /> HTML, and CSS.</div>
-                                            </div></Link>
-                                        }
+                                                    alt="Create-A-Playlist GIF" /> :
+                                                <Link to="/createaplaylist"><div>
+                                                    <img className="CAPvid" src="https://media.giphy.com/media/X96vlArkPHSbNUewCj/giphy.gif"
+                                                        alt="Create-A-Playlist GIF" />
+                                                    <div className="CAPdetails">Built with <br /> React, Node, <br /> HTML, and CSS.</div>
+                                                </div></Link>
+                                            }
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="project">
-                                    <h1 className='projectName'>Tybl</h1>
-                                    <img src={laptop} alt="" className="laptop" />
-                                    <div onMouseOver={() => this.showTybldetails()} onMouseOut={() => this.showTybldetailsOut()}>
-                                        {this.state.Tybldetails ?
-                                            <img className="CAPvid" src="https://media.giphy.com/media/2tSkUPstWNoXBuggpD/giphy.gif"
-                                                alt="Create-A-Playlist GIF" /> :
-                                            <Link to="/tybl"><div>
+                                    <div className="project">
+                                        <h1 className='projectName'>Tybl</h1>
+                                        <img src={laptop} alt="" className="laptop" />
+                                        <div onMouseOver={() => this.showTybldetails()} onMouseOut={() => this.showTybldetailsOut()}>
+                                            {this.state.Tybldetails ?
                                                 <img className="CAPvid" src="https://media.giphy.com/media/2tSkUPstWNoXBuggpD/giphy.gif"
-                                                    alt="Create-A-Playlist GIF" />
-                                                <div className="CAPdetails">Built with <br /> React, Redux, <br /> PostgresQL, and Auth0.</div>
-                                            </div></Link>
-                                        }
+                                                    alt="Create-A-Playlist GIF" /> :
+                                                <Link to="/tybl"><div>
+                                                    <img className="CAPvid" src="https://media.giphy.com/media/2tSkUPstWNoXBuggpD/giphy.gif"
+                                                        alt="Create-A-Playlist GIF" />
+                                                    <div className="CAPdetails">Built with <br /> React, Redux, <br /> PostgresQL, and Auth0.</div>
+                                                </div></Link>
+                                            }
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="project">
-                                    <h1 className='projectName'>Twitter Book</h1>
-                                    <img src={laptop} alt="" className="laptop" />
-                                    <div onMouseOver={() => this.showTwitterdetails()} onMouseOut={() => this.showTwitterdetailsOut()}>
-                                        {this.state.Twitterdetails ?
-                                            <img className="CAPvid gif" src="https://media.giphy.com/media/E0JteKnrvy0NZy2cCV/giphy.gif"
-                                                alt="Create-A-Playlist GIF" /> :
-                                            <Link to="/groupproject"><div>
+                                    <div className="project">
+                                        <h1 className='projectName'>Twitter Book</h1>
+                                        <img src={laptop} alt="" className="laptop" />
+                                        <div onMouseOver={() => this.showTwitterdetails()} onMouseOut={() => this.showTwitterdetailsOut()}>
+                                            {this.state.Twitterdetails ?
                                                 <img className="CAPvid gif" src="https://media.giphy.com/media/E0JteKnrvy0NZy2cCV/giphy.gif"
-                                                    alt="Create-A-Playlist GIF" />
-                                                <div className="CAPdetails">Built with <br /> React, Redux, <br /> PostgresQL, and Twitter API.</div>
-                                            </div></Link>
-                                        }
+                                                    alt="Create-A-Playlist GIF" /> :
+                                                <Link to="/groupproject"><div>
+                                                    <img className="CAPvid gif" src="https://media.giphy.com/media/E0JteKnrvy0NZy2cCV/giphy.gif"
+                                                        alt="Create-A-Playlist GIF" />
+                                                    <div className="CAPdetails">Built with <br /> React, Redux, <br /> PostgresQL, and Twitter API.</div>
+                                                </div></Link>
+                                            }
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <br />
                             <div id="contactme">
                                 <h1 className="contactmetitle">Contact Me</h1>
                                 <div className="divider"></div>
